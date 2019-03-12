93316b46faa2ee947810ba483c53209b84c6b236
Mikhail Chumachkin <chumachkin.m@gmail.com>
2019-03-12 17:18:07 +0200

    Fix form route and remove unnecessary methods from form controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::match(['get'], '/', 'MainFormController@index');
+Route::match(['post'], '/', 'MainFormController@store');
+
